b1b7ff0476c068d2bb14a85741677b63a613f59b
dmetro <dima.gorlov@gmail.com>
2016-01-28 00:04:57 +0200

    Initial commit

---
+++ App_Code/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+[RoutePrefix("api")]
+public class TestController : ApiController
+{
+    [HttpGet]
+    [Route("test")]
+    public IHttpActionResult Test()
+    {
+        return Ok(new
+        {
+            all = "Good!"
+        });
+    }
+}
